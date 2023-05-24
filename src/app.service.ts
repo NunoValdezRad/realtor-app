ad6eb7fda8ab1df18bc456910bf735571d14a352
NunoValdezRad <nuno.valdez@radically.digital>
2023-05-24 09:20:33 +0100

    feat/RA-01 docker compose added & prisma

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,9 +5,3 @@ export class AppService {
     
  
 }
-
-
-
-
-// 1) install postgress on local machine   running on localhost:4321...
-// 2) Host (create) postgress instance in the clowd - AWS, Azure, Heroku (vamos usar heroku, mais fácil) heroku.com - create accountls
